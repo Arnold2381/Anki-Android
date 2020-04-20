dc7cef5a0b3cf50f6756872674df4e370345d19f
David Allison <62114487+david-allison-1@users.noreply.github.com>
2020-04-20 11:15:32 +0100

    Convert Bundle to LargeObjectStorage
    To stop TransactionTooLargeException

--- AnkiDroid/src/main/java/com/ichi2/anki/NoteEditor.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/NoteEditor.java
@@ -34,6 +34,8 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.CheckResult;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -112,6 +114,8 @@ import com.ichi2.utils.AdaptionUtil;
 import com.ichi2.utils.HashUtil;
 import com.ichi2.utils.KeyUtils;
 import com.ichi2.utils.MapUtil;
+import com.ichi2.utils.LargeObjectStorage;
+import com.ichi2.utils.NamedJSONComparator;
 import com.ichi2.utils.NoteFieldDecorator;
 import com.ichi2.utils.TextViewUtil;
 import com.ichi2.widget.WidgetStatus;
@@ -287,6 +291,8 @@ public class NoteEditor extends AnkiActivity implements
         SAME_NUMBER,
         INCREMENT_NUMBER
     }
+
+    LargeObjectStorage mLargeObjectStorage = new LargeObjectStorage(this);
 
     private static class SaveNoteHandler extends TaskListenerWithContext<NoteEditor, Integer, Boolean> {
         private boolean mCloseAfter = false;
@@ -1342,9 +1348,6 @@ public class NoteEditor extends AnkiActivity implements
                 }
                 break;
             }
-            case REQUEST_VISUAL_EDIT:
-                requestEdit(REQUEST_VISUAL_EDIT, resultCode, data);
-                break;
             case REQUEST_MULTIMEDIA_EDIT: {
                 requestEdit(REQUEST_MULTIMEDIA_EDIT, resultCode, data);
                 break;
@@ -1561,16 +1564,33 @@ public class NoteEditor extends AnkiActivity implements
             try {
                 String value = mEditFields.get(index).getText().toString();
                 Intent i = new Intent(this, VisualEditorActivity.class);
-                i.putExtra(VisualEditorActivity.EXTRA_FIELD, value);
+                //Note: Intent.getExtras is a copy of the bundle.
+                Bundle b = new Bundle();
+                mLargeObjectStorage.storeSingleInstance(VisualEditorActivity.STORAGE_CURRENT_FIELD.asData(value), b);
+                mLargeObjectStorage.storeSingleInstance(VisualEditorActivity.STORAGE_EXTRA_FIELDS.asData(mEditorNote.getFields()), b);
+                i.replaceExtras(b);
+                i.putExtra(VisualEditorActivity.EXTRA_MODEL_ID, getModelId());
                 i.putExtra(VisualEditorActivity.EXTRA_FIELD_INDEX, index);
-                i.putExtra(VisualEditorActivity.EXTRA_ALL_FIELDS, mEditorNote.getFields());
-                i.putExtra(VisualEditorActivity.EXTRA_MODEL_ID, mEditorNote.model().getLong("id"));
-                startActivityForResultWithoutAnimation(i, REQUEST_VISUAL_EDIT);
+                launchActivityForResultWithAnimation(i, mVisualEditorLauncher, START);
             } catch (Exception e) {
-                UIUtils.showThemedToast(this, "Unable to open Visual Editor", false);
+                Timber.w(e, getString(R.string.unable_to_open_visual_editor));
+                UIUtils.showThemedToast(this, getString(R.string.unable_to_open_visual_editor), false);
             }
         });
     }
+
+
+    ActivityResultLauncher<Intent> mVisualEditorLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        Timber.d("VisualEditorActivityResult: resultCode=%d", result.getResultCode());
+        if (result.getResultCode() != RESULT_OK) {
+            return;
+        }
+        Intent data = result.getData();
+        if (data != null) {
+            requestEdit(REQUEST_VISUAL_EDIT, result.getResultCode(), data);
+        }
+    });
+
 
 
     private void setMMButtonListener(ImageButton mediaButton, final int index) {
